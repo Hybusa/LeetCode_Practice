a1c08d633e15069c5338aadbea424b6ad929c11c
Hybusa <alkouzne007@gmail.com>
2023-01-16 22:06:04 +0300

    Two sum

---
+++ src/LeetCode_01_Solution.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class LeetCode_01_Solution {
+    public int[] twoSumSolution1(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return nums;
+    }
+
+    public int[] twoSumSolution2(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (hashMap.containsKey(nums[i]))
+                return new int[]{hashMap.get(nums[i]), i};
+
+            hashMap.put(target - nums[i], i);
+        }
+        return nums;
+    }
+
+    public int[] twoSumSolution3(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            Integer complementIndex = hashMap.get(nums[i]);
+            if (complementIndex != null)
+                return new int[]{complementIndex, i};
+            hashMap.put(target - nums[i], i);
+
+        }
+        return nums;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LeetCode_01_Solution Sol1 = new LeetCode_01_Solution();
+        int [] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(Sol1.twoSumSolution1(nums, target)));
+        System.out.println(Arrays.toString(Sol1.twoSumSolution2(nums, target)));
+        System.out.println(Arrays.toString(Sol1.twoSumSolution3(nums, target)));
     }
 }
+
