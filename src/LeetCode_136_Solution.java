88ea2b5f7d53d4a4bc84f8f65d2bf4871324d863
Hybusa <alkouzne007@gmail.com>
2023-01-18 01:46:55 +0300

    Climb stairs

--- src/LeetCode_136_Solution.java
+++ src/LeetCode_136_Solution.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+
 public class LeetCode_136_Solution {
+
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for (int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+
+    public int singleNumber2(int[] nums) {
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+
+            if (nums[i] != nums[i + 1])
+                return nums[i];
+        }
+        return nums[nums.length - 1];
+    }
 }
